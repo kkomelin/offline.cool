9b4e8724162c7dfa5ca826cb17a4cd4d87d4ab04
Konstantin Komelin <konstantin.komelin@gmail.com>
2024-11-10 13:30:40 +0700

    Added umami tracking.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import "~~/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
+import "~~/styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />{" "}
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="b08ba708-7ba4-4334-9e7d-82f7ba9d3d1d"
+      />
+    </>
+  );
 }
